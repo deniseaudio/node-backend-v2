718a81704e4b7183258eb27685d1e28e80216d40
Thomas Cazade <cazade.thomas@gmail.com>
2022-09-23 11:40:52 +0200

    Bootstrap auth routes

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,11 +1,13 @@
 import { App } from "./App";
 import { validateEnv } from "./utils/validate-env";
+
 import { IndexRoute } from "./routes/IndexRoute";
+import { AuthRoute } from "./routes/AuthRoute";
 
 export const startServer = () => {
   validateEnv();
 
-  const app = new App([new IndexRoute()]);
+  const app = new App([new IndexRoute(), new AuthRoute()]);
 
   app.listen();
 
