d50d4d0719bb6c14c4a457c6f2c03dc947781495
Thomas Cazade <cazade.thomas@gmail.com>
2022-10-13 15:04:15 +0200

    Add low-bandwidth mode setting on User model

---
+++ prisma/migrations/20221013130217_added_low_bandwidth_user_setting/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "lowBandwidthEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "lowBandwidthBitrate" INTEGER NOT NULL DEFAULT 160
+);
+INSERT INTO "new_User" ("email", "id", "password", "username") SELECT "email", "id", "password", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
